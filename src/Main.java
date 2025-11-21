83bc35ba13aab6681703804273eecff9e1947666
Losker <loskertopgame@gmail.com>
2025-11-21 18:53:42 +0200

    убрал println для того что бы слиять ветку с дзшной

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Добро Пожаловать!");
         System.out.print("hello mir!");
         System.out.print("\n");
 
